434d656bb0730b8a8715fe4bcf6317667e67dbd8
Keyvan-RezaeiF <k1rzfi80@gmail.com>
2022-01-02 19:47:25 +0330

    ADD: Nav component

---
+++ src/firebase.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyDsOMgi4j1Duom8WKJSttbX4yPK_9C3LtY",
+    authDomain: "netflix-clone-eed53.firebaseapp.com",
+    projectId: "netflix-clone-eed53",
+    storageBucket: "netflix-clone-eed53.appspot.com",
+    messagingSenderId: "1046365558361",
+    appId: "1:1046365558361:web:8ba9c637d9a2f6e6a18348"
+  };
