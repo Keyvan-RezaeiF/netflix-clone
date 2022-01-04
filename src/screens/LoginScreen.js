3cc6a3ccaaa606cf3cde17ca36dff1cc6b253ccd
Keyvan-RezaeiF <k1rzfi80@gmail.com>
2022-01-04 16:00:35 +0330

    ADD: sign up screen

--- src/screens/LoginScreen.js
+++ src/screens/LoginScreen.js
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from 'react'
 import './LoginScreen.css'
+import SignUpScreen from './SignUpScreen';
 
 function LoginScreen() {
 
@@ -25,22 +26,26 @@ function LoginScreen() {
                 <div className='loginScreen_gradient' />
 
                 <div className='loginScreen_body'>
-                    <>
-                        <h1>Unlimited films, TV programmes and more.</h1>
-                        <h2>Watch anywhere, Cancel at any time.</h2>
-                        <h3>Ready to watch? Enter your email to create o restart your membership.</h3>
-                        <div className='loginScreen_input'>
-                            <form>
-                                <input type='email' placeholder='Email Address'/>
-                                <button
-                                    onClick={() => setSignIn(true)}
-                                    className='loginScreen_getStarted'
-                                >
-                                    Get Started
-                                </button>
-                            </form>
-                        </div>
-                    </>
+                    {signIn ? (
+                        <SignUpScreen />
+                    ) : (
+                        <>
+                            <h1>Unlimited films, TV programmes and more.</h1>
+                            <h2>Watch anywhere, Cancel at any time.</h2>
+                            <h3>Ready to watch? Enter your email to create o restart your membership.</h3>
+                            <div className='loginScreen_input'>
+                                <form>
+                                    <input type='email' placeholder='Email Address'/>
+                                    <button
+                                        onClick={() => setSignIn(true)}
+                                        className='loginScreen_getStarted'
+                                    >
+                                        Get Started
+                                    </button>
+                                </form>
+                            </div>
+                        </>
+                    )}
                 </div>
             </div>
         </div>
