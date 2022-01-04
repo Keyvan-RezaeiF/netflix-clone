8bc2f4a607f1dfc527e9400ca36ccb94dbc0b8ab
Keyvan-RezaeiF <k1rzfi80@gmail.com>
2022-01-04 12:24:52 +0330

    ADD: rows of different genres

--- src/HomeScreen.js
+++ src/HomeScreen.js
@@ -2,15 +2,23 @@ import React from 'react'
 import './HomeScreen.css'
 import Nav from './Nav'
 import Banner from './Banner'
+import Row from './Row'
+import requests from './Requests'
 
 function HomeScreen() {
     return (
         <div className='homeScreen'>
             <Nav />
-
             <Banner />
+            <Row title='NETFLIX ORIGINALS'fetchURL={requests.fetchNetflixOriginals} isLargeRow />
+            <Row title='TRENDING NOW'fetchURL={requests.fetchTrending} />
+            <Row title='TOP RATED'fetchURL={requests.fetchTopRated} />
+            <Row title='ACTION MOVIES'fetchURL={requests.fetchActionMovies} />
+            <Row title='COMEDY MOVIES'fetchURL={requests.fetchComedyMovies} />
+            <Row title='HORROR MOVIES'fetchURL={requests.fetchHorrorMovies} />
+            <Row title='ROMANCE MOVIES'fetchURL={requests.fetchRomanceMovies} />
+            <Row title='DOCUMENTARIES'fetchURL={requests.fetchDocumentaries} />
 
-            {/* Row */}
         </div>
     )
 }
