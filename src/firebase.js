223ac7222aa86dca191ae9b007e9c052e2768057
Keyvan-RezaeiF <k1rzfi80@gmail.com>
2022-01-04 16:31:30 +0330

    ADD: firebase auth

--- src/firebase.js
+++ src/firebase.js
@@ -1,3 +1,5 @@
+import firebase from 'firebase'
+
 const firebaseConfig = {
     apiKey: "AIzaSyDsOMgi4j1Duom8WKJSttbX4yPK_9C3LtY",
     authDomain: "netflix-clone-eed53.firebaseapp.com",
@@ -5,4 +7,12 @@ const firebaseConfig = {
     storageBucket: "netflix-clone-eed53.appspot.com",
     messagingSenderId: "1046365558361",
     appId: "1:1046365558361:web:8ba9c637d9a2f6e6a18348"
-  };
+};
+
+
+const firebaseApp = firebase.initializeApp(firebaseConfig);
+const db = firebase.firestore();
+const auth = firebase.auth();
+
+export { auth };
+export default db;
