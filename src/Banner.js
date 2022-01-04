38b932d80f324b9def3f1dde1e37597ea6a1e561
Keyvan-RezaeiF <k1rzfi80@gmail.com>
2022-01-04 11:40:32 +0330

    ADD: original banners

--- src/Banner.js
+++ src/Banner.js
@@ -1,7 +1,27 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import "./Banner.css"
+import axios from './axios'
+import requsets from "./Requests"
 
 function Banner() {
+
+    const [movie, setMovie] = useState([]);
+
+    useEffect(() => {
+        async function fetchData() {
+            const requset = await axios.get(requsets.fetchNetflixOriginals);
+            setMovie(
+                requset.data.results[
+                    Math.floor(Math.random() * requset.data.results.length - 1)
+                ]
+            );
+            return requset;
+        }
+
+        fetchData();
+    }, [])
+
+    console.log(movie);
 
     function truncate(string, n) {
         return string?.length > n ? string.substr(0, n - 1) + '...' : string
@@ -12,16 +32,16 @@ function Banner() {
           className='banner'
           style={{
             backgroundSize: "cover",
-            backgroundImage: `url("/images/black-banner.png")`,
-            backgroundPosition: "center center",
+            backgroundImage: `url("https://image.tmdb.org/t/p/original/${movie?.backdrop_path}")`,
+            // backgroundPosition: "center center",
         }}>
             <div className='banner_contents'>
-                <h1 className='banner_title'>Movie Name</h1>
+                <h1 className='banner_title'>{movie?.title || movie?.name || movie?.original_name}</h1>
                 <div className='banner_buttons'>
                     <button className='banner_button'>Play</button>
                     <button className='banner_button'>My List</button>
                 </div>
-                <h1 className='banner_description'>{truncate(`This is a descriptionThis is a descriptionThis is a descriptionThis is a descriptionThis is a descriptionThis is a descriptionThis is a descriptionThis is a description`, 150)}</h1>
+                <h1 className='banner_description'>{truncate(movie?.overview, 150)}</h1>
             </div>
             <div className='banner_fadeButtom' />
         </header>
