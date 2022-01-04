882bfddd80c6e48c141237dfcabf870769c7ef86
Keyvan-RezaeiF <k1rzfi80@gmail.com>
2022-01-05 00:52:30 +0330

    ADD: redux for login/logout

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React, { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
 import './App.css';
 import HomeScreen from './screens/HomeScreen';
 import {
@@ -8,18 +9,24 @@ import {
 } from "react-router-dom";
 import LoginScreen from './screens/LoginScreen'
 import { auth } from './firebase'
+import { login, logout, selectUser } from './features/userSlice';
 
 function App() {
 
-  const user = null;
+  const user = useSelector(selectUser);
+  const dispatch = useDispatch()
 
   useEffect(() => {
     const unsubscribe = auth.onAuthStateChanged(userAuth => {
       if (userAuth) {
         // Logged in
-        console.log(userAuth)
+        dispatch(login({
+          uid: userAuth.uid,
+          email: userAuth.email
+        }))
       } else {
         // Logged out
+        dispatch(logout)
       }
     });
 
@@ -33,7 +40,7 @@ function App() {
           <LoginScreen />
         ) : (
           <Routes>
-            <Route exaxt path='/' element={<HomeScreen />} />
+            <Route exact path='/' element={<HomeScreen />} />
           </Routes>
         )}
       </Router>
